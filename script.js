b5bac21619707fc37780ccb6c1a921a8f47b4c65
TheGreatMagnus <daisy@unitingnc.org>
2018-03-21 16:58:03 -0400

    human player can take a turn, clears board on button click

--- script.js
+++ script.js
@@ -13,15 +13,43 @@ const winner = [
   [0,4,8],
   [6,4,2]
 ]
-
-const cell = document.querySelectorAll('td')
-
-// logic to start the game
+//  //every cell on the board
+const cells = document.querySelectorAll('.cell')
+//
+// // logic to start the game
 startGame();
+
+// 1. removes styling from end game modal
+// 2. creates an array from the board
+// 3. loops through the array, removes the text inside the square,
+//    removes the background color, turns the turnClick to false
 function startGame() {
   document.querySelector(".endgame").style.display = "none";
-  originalBoard = Array.from(Array(9).keys())
+  originalBoard = Array.from(Array(9).keys());
+   for (var i = 0; i < cells.length; i++) {
+     cells[i].innerText = '';
+     cells[i].style.removeProperty('background-color');
+     cells[i].addEventListener('click', turnClick, false);
+   }
 }
+
+function turnClick(square){
+  turn(square.target.id, humanPlayer);
+}
+
+function turn(squareId, player) {
+  originalBoard[squareId] = player;
+  document.getElementById(squareId).innerText = player;
+  var gameWon = checkWin(originalBoard, player)
+    if (gameWon){
+      gameOver(gameWon)
+    }
+}
+
+// function checkWin(board, player) {
+//
+// }
+
 // logic to show winner
 
 // AI
